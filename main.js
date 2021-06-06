9b32bc5ba497efe3ee905680255f278a2a2dbdc2
sauravDutt <sauravd069@gmail.com>
2021-06-07 01:13:38 +0530

    Just added some more stuff

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ Array(1500).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+const spaceTexture = new THREE.TextureLoader({color: 0x1f1f1f});
 scene.background = spaceTexture;
 
 
